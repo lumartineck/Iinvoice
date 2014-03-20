f61201167e02594c74078104e64090e0f15249e2
Luis Enrique Martínez Gómez <lumartineck@gmail.com>
2014-03-19 22:23:25 -0600

    se arregla menu carousel

--- app/view/Menu.js
+++ app/view/Menu.js
@@ -31,21 +31,33 @@ Ext.define('Iinvoice.view.Menu', {
             startrecord= (i*itemsPerPage)-itemsPerPage;
             endrecord = i==totalpages ? Ext.getStore('Menu').getCount() : (i*itemsPerPage)-1;
             recordsforpage = Ext.getStore('Menu').getRange(startrecord,endrecord);
-            carouselpage = new Ext.dataview.DataView({
-                store: {
-                    fields: [
-                        {name: 'name', type: 'string'},
-                        {name: 'icon',  type: 'string'}
-                    ],
-                    data: recordsforpage
-                },
-                itemTpl: '<div style="margin:20px; width: 65px; height: 100px;"><img src="{icon}"/><br><span style="margin-left: 5px; font-size: 10px;">{name}</span></div>',
-                scrollable: true
-            });
+            carouselpage = {
+                xtype: 'container',
+                layout: 'fit',
+                items: [
+                    {
+                        xtype: 'dataview',
+                        scrollable: false,
+                        padding: 20,
+                        inline: true,
+                        cls: 'dataview-inline',
+                        store: {
+                            fields: [
+                                {name: 'name', type: 'string'},
+                                {name: 'icon',  type: 'string'}
+                            ],
+                            data: recordsforpage
+                        },
+                        itemTpl: '<div style="width: 65px; height: 100px;"><img src="{icon}"/><br><span style="font-size: 10px;text-align: center;">{name}</span></div>'
+                    }
+                ]
+            };
+
             carouselpages.push(carouselpage)
         }
 
         me.add(Ext.create('Ext.Carousel', {
+            layout: 'fit',
             fullscreen: true,
             items: carouselpages
         }));
